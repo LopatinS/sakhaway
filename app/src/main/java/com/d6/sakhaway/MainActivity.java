abd3415b4b3569d8b78f793dacb37ff7164e5880
LopatinS <wot262005@ya.ru>
2021-03-04 15:17:01 +0900

    data

--- app/src/main/java/com/d6/sakhaway/MainActivity.java
+++ app/src/main/java/com/d6/sakhaway/MainActivity.java
@@ -1,17 +1,8 @@
 package com.d6.sakhaway;
 
 import android.os.Bundle;
-import android.os.Handler;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
-import com.google.android.gms.common.util.concurrent.HandlerExecutor;
 
 public class MainActivity extends AppCompatActivity {
 
