38be631908cbb1cefe9a8b56f5e5da4360462f83
LopatinS <wot262005@ya.ru>
2021-03-04 14:03:42 +0900

    update

--- app/src/main/java/com/d6/sakhaway/FirstFragment.java
+++ app/src/main/java/com/d6/sakhaway/FirstFragment.java
@@ -22,7 +22,7 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.button_one).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(FirstFragment.this)
--- app/src/main/java/com/d6/sakhaway/MainActivity.java
+++ app/src/main/java/com/d6/sakhaway/MainActivity.java
@@ -1,14 +1,10 @@
 package com.d6.sakhaway;
 
 import android.os.Bundle;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.obratnaya_svyaz);
         fab.setOnClickListener(new View.OnClickListener() {
